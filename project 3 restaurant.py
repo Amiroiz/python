476f071d50b7947fd54f9f5621a739aa44f4cfd4
Amiroiz <barqala2001@gmail.com>
2024-12-13 11:56:39 +0330

    project

--- project 3 restaurant.py
+++ project 3 restaurant.py
@@ -1,16 +1,26 @@
 import requests
 import bs4
 
-url = "https://takhfifan.com/tehran/restaurants-cafes"
+
+url="https://takhfifan.com/tehran/restaurants-cafes"
 
 response = requests.get(url)
 
-soup = bs4.BeautifulSoup(response.text,'html.parser')
-restaurant = soup.find_all("p",attrs={"class":"vendor-card-box__title-text"})
-rating = soup.find_all('p',attrs={"class":"rate-badge__rate-value"})
+soup = bs4.BeautifulSoup(response.text,"html.parser")
 
-restaurant_data = []
-for i in restaurant:
-    name = restaurant
-    rating = rating
-    restaurant_data.append({'name': name, 'rating': rating})
+rate = soup.find_all("p",attrs={"class":"rate-badge__rate-value"})
+
+
+rates = []
+name = soup.find_all("p",attrs={"class":"vendor-card-box__title-text"})
+names = []
+for i,x in zip(rate,name):
+    thescore=i.text.strip()
+    rates.append(float(thescore))
+    thename=x.text.strip()
+    names.append(thename)
+
+max_rate = max(rates)
+max_index = rates.index(max_rate)
+max_name = names[max_index]
+print("Max Restaurant Name :", max_name ,"--->","Rate :", max_rate )
